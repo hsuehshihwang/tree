51d453ae18d0cb7e9b1a9e486dc6a75cd73c264f
Ralph Wang <enhanceralph@gmail.com>
2025-08-25 15:41:35 +0800

    Update

--- work/skills/nginx/netns/current.sh
+++ work/skills/nginx/netns/current.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Current Folder: $(pwd)"
+curr=$(pwd)
+echo "Current Folder: .${curr#*/tree}"
 echo -ne "Current Sub-folders: "
 find . -maxdepth 1 -type d ! -path . ! -path "*.git*" | sed -e "s%^./%%" | awk '{printf $1" "} END{printf "\n"}'
 howdeep=$(find . -type d | awk -F/ '{print NF-1}' | sort -n | tail -1)

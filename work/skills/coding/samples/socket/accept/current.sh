66ac1dba78d5cb0fd9e1439b8524c68cf6cb055d
Ralph Wang <enhanceralph@gmail.com>
2025-08-25 15:29:34 +0800

    Update

--- work/skills/coding/samples/socket/accept/current.sh
+++ work/skills/coding/samples/socket/accept/current.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo -ne "Current Folders: "
+echo "Current Folder: $(pwd)"
+echo -ne "Current Sub-folders: "
 find . -maxdepth 1 -type d ! -path . ! -path "*.git*" | sed -e "s%^./%%" | awk '{printf $1" "} END{printf "\n"}'
 howdeep=$(find . -type d | awk -F/ '{print NF-1}' | sort -n | tail -1)
 # echo howdeep=$howdeep
